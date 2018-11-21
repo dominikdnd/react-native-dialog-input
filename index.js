d7b4209a6a2eff17603335e39ccff8d4214db585
Dominik <39653465+dominikking@users.noreply.github.com>
2018-11-21 14:08:43 +0100

    Update index.js
    Add value prop.

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ class DialogInput extends React.Component{
                 underlineColorAndroid='transparent'
                 placeholder={hintInput}
                 onChangeText={(inputModal) => this.setState({inputModal})}
-		value={this.state.inputModal}
+		value={this.state.inputModal || this.props.value}
                 />
             </View>
             <View style={styles.btn_container}>
